6464bfb4ba96b5360510cd6f32fd87ff2c7e7743
Latfiansya <latfiansyadzikri@gmail.com>
2024-10-08 10:31:40 +0700

    first commit

---
+++ assigment1.py
@@ -0,0 +1,22 @@
+#1.Print your name to the console.
+#2.Print the sentence "I am learning Python basics." on a new line.
+#3.Write a program that asks the user for their name and then greets them with "Hello, [Name]!".
+#4.Above every line of code or block of code you wrote in Task 3, write a comment explaining what the code does.
+#5.Write a multi-line comment at the beginning of the code describing the purpose of the program below:
+
+#1
+print("Latfiansya Dzikri")
+
+#2
+print("I am learning Python basics.")
+
+#3 & 4
+#membuat variabel name sebagai tempat menyimpan nama
+name = input("Siapa namamu? ")
+#cetak/output data yang telah di input dan menambah kalimat/kata disebelum variabel dipanggil
+print("Hello,", name)
+
+#5
+#program meminta untuk input data umur kemudian mencetak/menampilkan data tsb.
+age = input("How old are you? ")
+print(age)
